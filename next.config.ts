9943530e78a89be04ad930bbe14b9678715ad6fc
wangsa <dewanggawpw@gmail.com>
2025-05-11 00:05:37 +0700

    setup eslint & prettier, auth layout, reusable authform

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbopack: false, // Disable Turbopack
+  } as any,
 };
 
 export default nextConfig;
